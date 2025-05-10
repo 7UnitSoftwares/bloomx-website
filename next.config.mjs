02aa42241649c301c9efabc99152d7e97152be5e
DIPIN KRISHNAN <dkrishnan@DIPINs-MacBook-Pro.local>
2025-05-10 12:47:43 +0530

    fixing docker build with env

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    env: {
+        NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+        NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+    }
 }
 
 export default nextConfig;
