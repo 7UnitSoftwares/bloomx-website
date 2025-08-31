c6f3a515da1bd3a6c60dcacb0ecb66b02cd2b429
Dipin Kumar Krishnan <dkrishnan@StudioDesk.local>
2025-08-31 22:31:17 +0530

    day change homepage

--- src/data/SpecialEventData.js
+++ src/data/SpecialEventData.js
@@ -3,7 +3,7 @@ export const SpecialEventData = [
     id: 1,
     title: "Aperi Bloom Open Day!",
     description: "Un pomeriggio speciale per divertirti, scoprire le nostre attività e lasciarti sorprendere dalle novità che abbiamo preparato per te!",
-    date: "Martedì 3 Settembre",
+    date: "Mercoledì 3 Settembre",
     duration: "ore 17:30",
     badge: "Evento Gratuito",
     image: "/event/open-day.jpg",
